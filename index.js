127f64b3d409f82b798d12ecd0e9c16b225af8e7
James Gary <bucket@earthlink.net>
2018-01-15 22:31:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine, newPeople){
 }
 
 function nowServing(katzDeliLine){
-  if (katzDeliLine === []){
+  if (katzDeliLine.length === 0){
     return 'There is nobody waiting to be served!';
   }
   else
